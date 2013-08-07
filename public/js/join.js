2322fc3ab65be4a2981c88f677922527843a797d
Mike Nolan <mnolan@siteworx.com>
2013-08-07 15:39:25 -0400

    additional ajax functionality

--- public/js/join.js
+++ public/js/join.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	getGames();
+	$('.games-button').click( joinGame );
 });
 
 function getGames() {
@@ -18,11 +19,27 @@ function getGames() {
 function update( data ) {
 	$('#games-body').empty();
 	for (var i = 0; i < data.length; i++) {
-		var game = data[i];
-		var html = "<tr><td class='game'>" + game.name
+		var game = data[i],
+			html = "<tr><td class='game'>" + game.name
 			+ "</td><td class='games-button'><input type='button' "
-			+ "class='btn btn-default' onclick='return login(this)' "
-			+"value='Join Game' id='" + i + "' /></td></tr>"
+			+ "class='btn btn-default' value='Join Game' id='" 
+			+ i + "' /></td></tr>"
 		$('#games-body').append( html );
 	};
+}
+
+function joinGame() {
+	var self = $(this),
+		url = '/game/' + self.attr('id') + '/participants'
+	$.ajax({
+		url: '/game',
+		method: 'POST',
+		success: function(data, textStatus, jqXHR) {
+			var gameUrl = '/index.html?id=' + self.attr('id');
+			window.location(gameUrl);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log('ERROR: ', errorThrown);
+		}
+	});	
 }
