4d6ed1cb9cdb7e483fead8c199a6da8edd84b147
Mike Nolan <mnolan@siteworx.com>
2013-08-07 15:22:21 -0400

    additional ajaxing

--- public/js/join.js
+++ public/js/join.js
@@ -19,7 +19,7 @@ function update( data ) {
 	$('#games-body').empty();
 	for (var i = 0; i < data.length; i++) {
 		var game = data[i];
-		var html = "<tr><td class='game'>" + data.name
+		var html = "<tr><td class='game'>" + game.name
 			+ "</td><td><input type='button' class='btn btn-default' "
 			+ "onclick='return login(this)'  value='Join Game' "
 			+"id='" + i + "' /></td></tr>"
